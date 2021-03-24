a39df7078d1a4e673bc1980c07be796f66031640
DDraeger1 <74009750+DDraeger1@users.noreply.github.com>
2021-03-24 08:13:53 +0100

    added contributers footer

--- src/staticComps/Footer.js
+++ src/staticComps/Footer.js
@@ -5,15 +5,24 @@ const useStyles = makeStyles((theme) => ({
         top: "100%",
         width: "100%",
         height: "23vh",
-        backgroundColor: "currentcolor"
+        backgroundColor: "currentcolor",
+        display:"grid",
+        gridTemplateColumns:"25% 25% 35% 15%",
+        gridAutoRows:"23vh"
     },
+    spoofy:{
+        gridArea:"0 / 4 / 0 / 4"
+    }
 }))
 function Footer() {
     const classes = useStyles()
         return (
             <div className={classes.footer}>
-                <a href="https://open.spotify.com/artist/1DLBs2535MM32RYqirYYY4?si=2j_tBrZKQoCLSsz8gnz4Ng">
+                <a className={classes.spoofy} href="https://open.spotify.com/artist/1DLBs2535MM32RYqirYYY4?si=2j_tBrZKQoCLSsz8gnz4Ng">
                     <img src={spoofy} alt="spoofy" width="300" height="200"></img>
+                </a>
+                <a href="https://github.com/GrumpSiteProject/MainWebsite/blob/master/README.md">
+                    <p>contributers</p>
                 </a>
             </div>
         )
