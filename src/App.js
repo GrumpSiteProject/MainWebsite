3ca138abd96b59829fa00f998a72cf700ddf8673
Max <maxacaplan@gmail.com>
2021-03-26 15:54:36 -0300

    Reorganized project files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
-import "./css/grumpBackground.css"
-import './App.css';
+import "./assets/css/main.css";
+import "./assets/css/grumpBackground.css";
+
 import { Route, Switch } from "react-router-dom";
-import MainPage from "./comps/MainPage"
-import Footer from "./staticComps/Footer"
-import Header from "./staticComps/Header"
-import Theater from "./comps/theatrePage/Theatre"
+
+import ThankYouMessages from "./comps/ThankYouMessages";
+import Theater from "./comps/theatrePage/Theatre";
+
+import Navbar from "./staticComps/Navbar";
+import Footer from "./staticComps/Footer";
+import Header from "./staticComps/Header";
+
+import PageNotFound from "./staticComps/PageNotFound";
+
 function App() {
   return (
+    <div className="app">
+      {/* <Switch>
+        <Route exact path="/">
+          <div className="page-container">
+            <div className="content-wrap">
+              <div className="bg-gradient">
+                <div className="bg-dots">
+                  <Header />
 
-    <div class="bg-gradient">
-      <div class="bg-dots">
-        <Switch>
-          <Route exact path="/">
-            <Header />
-            <MainPage />
+                  <MainPage />
+                </div>
+              </div>
+            </div>
+
             <Footer />
-          </Route>
-          <Route path="/theatre">
+          </div>
+        </Route>
 
-            <Theater />
+        <Route path="/theatre">
+          <div className="bg-gradient">
+            <div class="bg-dots">
+              <Theater />
+
+              <Footer />
+            </div>
+          </div>
+        </Route>
+      </Switch> */}
+
+      <Switch>
+        <Route exact path="/">
+          <div className="page-container">
+            <div className="content-wrap">
+              <div className="bg-gradient">
+                <div className="bg-dots">
+                  <Navbar />
+
+                  <ThankYouMessages />
+
+                  <div className="footer-push"></div>
+                </div>
+              </div>
+            </div>
 
             <Footer />
-          </Route>
-        </ Switch>
-      </div>
+          </div>
+        </Route>
+
+        <Route path="/theatre">
+          <div className="page-container">
+            <div className="content-wrap">
+              <div className="bg-gradient">
+                <div className="bg-dots">
+                  <Navbar />
+
+                  <Theater />
+
+                  <div className="footer-push"></div>
+                </div>
+              </div>
+            </div>
+
+            <Footer />
+          </div>
+        </Route>
+
+        <Route path="*">
+          <div className="page-container">
+            <div className="content-wrap">
+              <div className="bg-gradient">
+                <div className="bg-dots">
+                  <Navbar />
+
+                  <PageNotFound />
+
+                  <div className="footer-push"></div>
+                </div>
+              </div>
+            </div>
+
+            <Footer />
+          </div>
+        </Route>
+      </Switch>
     </div>
-
   );
 }
 
